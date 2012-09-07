d35a25d8f368c8dd33a3af0d07c9dc273e74aff3
Gavin L <puttabutta@gmail.com>
2012-09-07 02:41:34 -0400

    Added missing files

---
+++ gtkasync.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+#------------------------------------------------------------------------------
+# gtk-async - Allows the use of asyncore with GTK's mainloop
+# Copyright (c) 2012 Gavin Langdon <puttabutta@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#------------------------------------------------------------------------------
+
+
+import asyncore
+
+from gi.repository import GObject
+
+def _loop():
+  asyncore.loop(0, False, None, 1)
+  return True
+
+def install():
+  GObject.idle_add(_loop)
